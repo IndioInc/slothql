fc0d3a49216f263b75d36d14a41fcae5556d64a1
karol-gruszczyk <karol.gruszczyk@gmail.com>
2018-01-25 14:04:45 -0800

    add django import check

--- slothql/__init__.py
+++ slothql/__init__.py
@@ -2,3 +2,12 @@ from .fields import *
 from .types import *
 from .schema import Schema
 from .query import gql
+
+try:
+    from slothql import django
+except ImportError:
+    try:  # check if the ImportError was actually caused by django
+        import django
+        raise
+    except ImportError:
+        pass
